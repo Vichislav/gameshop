3c016adb932d8b01d67639ceedf3b7f00e036904
Vichislav <s1ava8968@gmail.com>
2024-09-01 12:22:55 +0600

    new app

---
+++ app/basket/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Basket() {
+  return (
+    <section className="flex justify-center align-middle min-h-screen min-w-[100%] bg-slate-700">
+        <h1>
+            Привет пес
+        </h1>
+    </section>
+  );
+}
