de440ec565a0e53d3ddce6a5d7d8839cf63e3619
Vichislav <s1ava8968@gmail.com>
2024-09-04 22:30:45 +0600

    routing work

--- app/basket/page.tsx
+++ app/basket/page.tsx
@@ -1,11 +1,19 @@
+'use client'
+import { useRouter } from "next/navigation";
 
 
 export default function Basket() {
+  const router = useRouter(); 
+
+  const goToHome = () => { 
+      router.push('/'); 
+  }; 
   return (
-    <section className="flex justify-center align-middle min-h-screen min-w-[100%] bg-slate-700">
+    <section className="flex flex-col w-[100%] gap-[20px] mt-[20px] justify-center items-center min-h-screen min-w-[100%] bg-slate-700">
         <h1>
             Привет пес
         </h1>
+        <button className='w-[120px] p-[5px] border-2 bg-slate-300' onClick={goToHome}>Back to home</button>
     </section>
   );
 }
--- app/first/components/index.tsx
+++ app/first/components/index.tsx
@@ -2,6 +2,8 @@
  
 import { useEffect, useState } from 'react'
 import { User } from '../page'
+import { useRouter } from 'next/navigation';
+import React from 'react';
 
 interface UserListProps {
     dataProps: User[];
@@ -10,6 +12,11 @@ interface UserListProps {
 export default function Grid ({ dataProps }: UserListProps)  {
 
     const [data, setData] = useState<User[] | null>(null)
+    const router = useRouter(); 
+
+    const goToBasket = () => { 
+        router.push('/basket'); 
+    }; 
     
     console.log('data')
     console.log(dataProps)
@@ -22,10 +29,11 @@ export default function Grid ({ dataProps }: UserListProps)  {
     <main className="flex flex-col items-center justify-between p-24">
       <h1 className="text-[50px]">Grid for data</h1>
         {data && data.map((item)=>{
-           return(<div>
+           return(<div key={item.id}>
                 {item.name}
             </div>) 
         })}
+        <button className='w-[120px] p-[5px] border-2 bg-slate-300' onClick={goToBasket}>to Basket</button>
     </main>
   );
 }
