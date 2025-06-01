c800d2ffa13fd7401056a3bc1c1eccf36b21b6a6
Vichislav <s1ava8968@gmail.com>
2025-06-01 09:13:35 +0600

    new change for operators tusk

--- app/tasks/layout.tsx
+++ app/tasks/layout.tsx
@@ -9,14 +9,14 @@ export default function TaskLayout({
   }) {
     return (
             <div className="flex flex-col lg:flex-row w-full p-2">
-                <nav className="flex flex-row lg:flex-col gap-2 w-[100%] lg:w-[25%]">
-                  <Link href={'/tasks'} className="w-[25%] lg:w-auto">
+                <nav className="flex flex-row lg:flex-col gap-2 w-[100%] lg:w-[25%] justify-center lg:justify-start">
+                  <Link href={'/tasks'} className="w-[30%] lg:w-auto">
                     <TusksTab name={'tasks'}></TusksTab>
                   </Link>
-                  <Link href={'/tasks/operators'} className="w-[25%] lg:w-auto">
+                  <Link href={'/tasks/operators'} className="w-[30%] lg:w-auto">
                     <TusksTab name={'operators'}></TusksTab>
                   </Link>
-                  <Link href={'/tasks/eventloop'} className="w-[25%] lg:w-auto">
+                  <Link href={'/tasks/eventloop'} className="w-[30%] lg:w-auto">
                     <TusksTab name={'eventloop'}></TusksTab>
                   </Link>
                 </nav>
--- app/tasks/operators/page.tsx
+++ app/tasks/operators/page.tsx
@@ -14,83 +14,61 @@ export default function Operators() {
 
   const router = useRouter();
 
-  const [data, setData] = useState<Item[]>([
-    { id: 0, text: '>' },
-    { id: 1, text: '&&' },
-    { id: 2, text: '||' },
-    { id: 3, text: '...' },
-    { id: 4, text: '...' },
-    { id: 5, text: '...' },
-  ]);
+  
 
   // Сохраняем id перетаскиваемого элемента
-  const [draggedId, setDraggedId] = useState<number | null>(null);
-  const [currentItem, setCurrentItem] = useState<Item | null>(null);
-  const [solving, setSolving] = useState<boolean>(false)
+ 
+  const [currentValue, setCurrentValue] = useState<string>('');
+  const [leftActive, setLeftActive] = useState<boolean>(false) //подсветить ли поле выбора элементов
+  const [rightActive, setRightActive] = useState<boolean>(false) //подсветить ли основное выражение, куда вставлять значение
+  const [isSetValue, setIsSetValue] = useState<boolean>(false) //задано ли текущие значение
+  const [solving, setSolving] = useState<boolean>(false)// верно ли решено задание
+//one two three
+  const [data, setData] =  useState<string[]>(['...', '...', '...']);
+  const [borederChange, setBorderChange] = useState<boolean[]>([false, false, false])
 
-  const onDragStart = (e: React.DragEvent<HTMLDivElement>, item: Item) => {
-    setCurrentItem(item)
-  };
-
-  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault(); // обязательно, чтобы разрешить drop
-    //e.dataTransfer.dropEffect = 'move';
-    const target = e.target as HTMLDivElement; // приведение типа
-    target.style.background = 'lightgray';
-
-  };
-
-  const onDrop = (e: React.DragEvent<HTMLDivElement>, item: Item) => {
-    e.preventDefault();
-    const target = e.target as HTMLDivElement; // приведение типа
-    target.style.background = '#f0f0f0';
-    setData(data.map(el => {
-      if (currentItem) {
-        if (el.id === item.id) {
-          return { ...el, id: currentItem.id }
-        }
-        if (el.id === currentItem.id) {
-          return { ...el, id: item.id }
-        }
-      }
-      return el
-    }));
-  };
-
-  const sortElem = (a: Item, b: Item) => {
-    if (a.id > b.id) {
-      return 1
-    } else {
-      return -1
-    }
+  const itemHandler = (str:string, ind: number) => {
+    setCurrentValue(str)//берем с кнопки текущее значение оператора
+    setBorderChange(() => {
+      const newArr = [false, false, false]
+      newArr[ind] = true
+      return newArr
+    })
+    setRightActive(true) //подсвечиваем правую часть зеленым
+    setIsSetValue(true) // говорим о том что значение взято
+    setTimeout(() => setRightActive(false), 1000);
   }
 
-  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-    const target = e.target as HTMLDivElement; // приведение типа
-    target.style.background = 'white';
-  };
+  const dataHandler = (index: number) => {
+    //устанавливаем значение по индексу
+    setData(data => {
+      const newData = [...data]
+      newData[index] = currentValue
+      return newData
+    })
+    //убираем выделение с кнопки панели выбора оператора
+    setIsSetValue(false)
+  }
 
-  const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    const target = e.target as HTMLDivElement; // приведение типа
-    target.style.background = 'white';
-  };
+
 
   useEffect(() => {
 
     const expression = `1&&9>2||2`;
-    const currentSolving = `1${data[3].text}9${data[4].text}2${data[5].text}2`
+    const currentSolving = `1${data[0]}9${data[1]}2${data[2]}2`
+    console.log(currentSolving)
     if (currentSolving === expression) {
       setSolving(true);
 
     } else {
       setSolving(false);
     }
-  }, [onDrop])
+  }, [data])
 
   const goToHome = () => {
     router.push('/');
   };
-
+ //className={`border-2 rounded p-2 border-gray-300 ${ isHighlighted ? 'animate-highlight' : ''}`}
 
   return (
     
@@ -99,60 +77,69 @@ export default function Operators() {
         min-h-screen min-w-[100%] bg-gradient-to-tr from-slate-300 to-slate-100 gree
         p-4">
 
-        <h1 className=' text-center' >Переместите элементы справа на места с &quot;...&quot; таким образом что бы выражение в итоге возвращало true</h1>
+        <h1 className=' text-center' >Переместите элементы справа на места слева &quot;_&quot; таким образом что бы выражение в итоге возвращало true</h1>
 
-        <div className='w-[100%] lg:w-[80%] flex items-center gap-[10px] border-2  border-black rounded-lg p-5'
+        <div className='w-[100%] lg:w-[80%] flex flex-col lg:flex-row items-center gap-[10px] border-2  border-black rounded-lg p-5'
           style={{ backgroundColor: solving ? '#7efca0' : 'white' }}
         >
 
-          <div className='flex flex-col border-r-2 w-[40px] border-black gap-2'>
-            {data.sort(sortElem).slice(0, 3).map(item => (
-              <div
-                key={item.id}
-                draggable
-                onDragStart={e => onDragStart(e, item)}
-                onDragOver={onDragOver}
-                onDrop={e => onDrop(e, item)}
-                onDragEnd={e => onDragEnd(e)}
-                onDragLeave={e => onDragLeave(e)}
-                className='flex justify-center items-center w-[35px] border-2 border-black rounded-lg cursor-grab'
-              >
-                {item.text}
+          <div className='flex flex-row border-b-2 lg:flex-col lg:border-r-2 lg:border-b-0 w-[90%] lg:w-[60px] border-black gap-6 lg:gap-2 justify-center  p-2'>
+           
+              <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${ borederChange[0] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
+              onClick={()=>itemHandler('>', 0)}>
+                &gt;
               </div>
-            ))}
+              <div  className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${ borederChange[1] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
+              onClick={()=>itemHandler('||', 1)}>
+                ||
+               </div>
+               <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${ borederChange[2] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
+              onClick={()=>itemHandler('&&', 2)}>
+                &&
+               </div>
+           
           </div>
 
-          <div className='flex h-[33%] gap-[30px]'>
-            <div className='flex justify-center items-center w-[35px] rounded-lg bg-white'>
+          <div className='flex h-[43%] gap-[2px] relative'>
+            <div className='flex justify-center items-center w-[40px] h-[40px] lg:w-[45px] lg:h-[45px] rounded-lg bg-white'>
               <p>1</p>
             </div>
-            <div className='flex justify-center items-center w-[35px] rounded-lg bg-white'>
+
+            <div 
+            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+            cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
+            onClick={()=>dataHandler(0)}>
+              {data[0]}
+            </div>
+
+            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white z-0'>
               <p>9</p>
             </div>
-            <div className='flex justify-center items-center w-[35px] rounded-lg bg-white'>
-              <p>2</p>
-            </div>
-            <div className='flex justify-center items-center w-[35px] rounded-lg bg-white'>
-              <p>2</p>
-            </div>
-          </div>
 
-          <div className='absolute flex  gap-[30px] z-10 translate-x-[85px]'>
-            {data.slice(3).map(item => (
-              <div
-                key={item.id}
-                draggable
-                onDragStart={e => onDragStart(e, item)}
-                onDragOver={onDragOver}
-                onDrop={e => onDrop(e, item)}
-                onDragEnd={e => onDragEnd(e)}
-                onDragLeave={e => onDragLeave(e)}
-                className='flex justify-center items-center w-[35px] border-2 border-black rounded-lg cursor-grab'
-                style={{ backgroundColor: draggedId === item.id ? '#f0f0f0' : 'white' }}
-              >
-                {item.text}
-              </div>
-            ))}
+            <div
+             className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+             cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
+             onClick={()=>dataHandler(1)}>
+              {data[1]}
+            </div>
+
+            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
+              <p>2</p>
+            </div>
+
+            <div  
+            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+            cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
+            onClick={()=>dataHandler(2)}>
+              {data[2]}
+            </div>
+
+            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
+              <p>2</p>
+            </div>
           </div>
           {solving && <div className=' p-2'>true!</div>}
         </div>
