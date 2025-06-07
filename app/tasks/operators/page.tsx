ee245fb175dfcfe25862705bbd488e825c7aa550
Vichislav <s1ava8968@gmail.com>
2025-06-07 19:00:48 +0600

    added new content for tusk page

--- app/tasks/operators/page.tsx
+++ app/tasks/operators/page.tsx
@@ -14,20 +14,20 @@ export default function Operators() {
 
   const router = useRouter();
 
-  
+
 
   // Сохраняем id перетаскиваемого элемента
- 
+
   const [currentValue, setCurrentValue] = useState<string>('');
   const [leftActive, setLeftActive] = useState<boolean>(false) //подсветить ли поле выбора элементов
   const [rightActive, setRightActive] = useState<boolean>(false) //подсветить ли основное выражение, куда вставлять значение
   const [isSetValue, setIsSetValue] = useState<boolean>(false) //задано ли текущие значение
   const [solving, setSolving] = useState<boolean>(false)// верно ли решено задание
-//one two three
-  const [data, setData] =  useState<string[]>(['...', '...', '...']);
+  //one two three
+  const [data, setData] = useState<string[]>(['...', '...', '...']);
   const [borederChange, setBorderChange] = useState<boolean[]>([false, false, false])
 
-  const itemHandler = (str:string, ind: number) => {
+  const itemHandler = (str: string, ind: number) => {
     setCurrentValue(str)//берем с кнопки текущее значение оператора
     setBorderChange(() => {
       const newArr = [false, false, false]
@@ -40,14 +40,29 @@ export default function Operators() {
   }
 
   const dataHandler = (index: number) => {
-    //устанавливаем значение по индексу
-    setData(data => {
-      const newData = [...data]
-      newData[index] = currentValue
-      return newData
-    })
-    //убираем выделение с кнопки панели выбора оператора
-    setIsSetValue(false)
+    
+    //если значение оператора утсановлено то 
+    if (isSetValue) {
+      //устанавливаем значение по индексу
+      setData(data => {
+        const newData = [...data]
+        newData[index] = currentValue
+        return newData
+      })
+      //убираем флаг того, что значение оператора выбрано
+      setIsSetValue(false)
+      //убираем текущие значение оператора 
+      setCurrentValue('...')
+      //убираем выделение с кнопки панели выбора оператора
+      setBorderChange(() => {
+        const newArr = [false, false, false]
+        return newArr
+      })
+    }else { //если значение я нет подсвечиваем операторы красным
+      setLeftActive(true)
+      setTimeout(() => setLeftActive(false), 1000);
+    }
+
   }
 
 
@@ -68,85 +83,88 @@ export default function Operators() {
   const goToHome = () => {
     router.push('/');
   };
- //className={`border-2 rounded p-2 border-gray-300 ${ isHighlighted ? 'animate-highlight' : ''}`}
+  //className={`border-2 rounded p-2 border-gray-300 ${ isHighlighted ? 'animate-highlight' : ''}`}
 
   return (
-    
 
-      <section className="flex flex-col w-[100%] gap-[20px] justify-start items-center 
+
+    <section className="flex flex-col w-[100%] gap-[20px] justify-start items-center 
         min-h-screen min-w-[100%] bg-gradient-to-tr from-slate-300 to-slate-100 gree
         p-4">
 
-        <h1 className=' text-center' >Переместите элементы справа на места слева &quot;_&quot; таким образом что бы выражение в итоге возвращало true</h1>
+      <h1 className=' text-center' >Переместите элементы справа на места слева &quot;_&quot; таким образом что бы выражение в итоге возвращало true</h1>
 
-        <div className='w-[100%] lg:w-[80%] flex flex-col lg:flex-row items-center gap-[10px] border-2  border-black rounded-lg p-5'
-          style={{ backgroundColor: solving ? '#7efca0' : 'white' }}
-        >
+      <div className='w-[100%] lg:w-[80%] flex flex-col lg:flex-row items-center gap-[10px] border-2  border-black rounded-lg p-5'
+        style={{ backgroundColor: solving ? '#7efca0' : 'white' }}
+      >
 
-          <div className='flex flex-row border-b-2 lg:flex-col lg:border-r-2 lg:border-b-0 w-[90%] lg:w-[60px] border-black gap-6 lg:gap-2 justify-center  p-2'>
-           
-              <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
-              cursor-pointer ${ borederChange[0] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
-              onClick={()=>itemHandler('>', 0)}>
-                &gt;
-              </div>
-              <div  className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
-              cursor-pointer ${ borederChange[1] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
-              onClick={()=>itemHandler('||', 1)}>
-                ||
-               </div>
-               <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
-              cursor-pointer ${ borederChange[2] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}`} 
-              onClick={()=>itemHandler('&&', 2)}>
-                &&
-               </div>
-           
+        <div className='flex flex-row border-b-2 lg:flex-col lg:border-r-2 lg:border-b-0 w-[90%] lg:w-[60px] border-black gap-6 lg:gap-2 justify-center  p-2'>
+
+          <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${borederChange[0] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}
+              ${leftActive ? 'animate-redlight' : ''}`}
+            onClick={() => itemHandler('>', 0)}>
+            &gt;
+          </div>
+          <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${borederChange[1] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}
+              ${leftActive ? 'animate-redlight' : ''}`}
+            onClick={() => itemHandler('||', 1)}>
+            ||
+          </div>
+          <div className={`flex justify-center items-center w-[45px] h-[45px]  rounded-lg 
+              cursor-pointer ${borederChange[2] ? 'border-2 border-dashed border-gray-600 bg-[#dde4ec]' : 'border-2 border-black bg-white'}
+              ${leftActive ? 'animate-redlight' : ''}`}
+            onClick={() => itemHandler('&&', 2)}>
+            &&
           </div>
 
-          <div className='flex h-[43%] gap-[2px] relative'>
-            <div className='flex justify-center items-center w-[40px] h-[40px] lg:w-[45px] lg:h-[45px] rounded-lg bg-white'>
-              <p>1</p>
-            </div>
-
-            <div 
-            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
-            cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
-            onClick={()=>dataHandler(0)}>
-              {data[0]}
-            </div>
-
-            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white z-0'>
-              <p>9</p>
-            </div>
-
-            <div
-             className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
-             cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
-             onClick={()=>dataHandler(1)}>
-              {data[1]}
-            </div>
-
-            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
-              <p>2</p>
-            </div>
-
-            <div  
-            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
-            cursor-pointer ${ rightActive ? 'animate-highlight' : ''}`}
-            onClick={()=>dataHandler(2)}>
-              {data[2]}
-            </div>
-
-            <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
-              <p>2</p>
-            </div>
-          </div>
-          {solving && <div className=' p-2'>true!</div>}
         </div>
-        <button className='w-[120px] p-[5px] border-2 bg-slate-300' onClick={goToHome}>Back to home</button>
-      </section>
 
-    
+        <div className='flex h-[43%] gap-[2px] relative'>
+          <div className='flex justify-center items-center w-[40px] h-[40px] lg:w-[45px] lg:h-[45px] rounded-lg bg-white'>
+            <p>1</p>
+          </div>
+
+          <div
+            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+            cursor-pointer ${rightActive ? 'animate-highlight' : ''}`}
+            onClick={() => dataHandler(0)}>
+            {data[0]}
+          </div>
+
+          <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white z-0'>
+            <p>9</p>
+          </div>
+
+          <div
+            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+             cursor-pointer ${rightActive ? 'animate-highlight' : ''}`}
+            onClick={() => dataHandler(1)}>
+            {data[1]}
+          </div>
+
+          <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
+            <p>2</p>
+          </div>
+
+          <div
+            className={`flex justify-center items-center w-[40px]  lg:w-[45px] border-2 border-black z-10 rounded-lg 
+            cursor-pointer ${rightActive ? 'animate-highlight' : ''}`}
+            onClick={() => dataHandler(2)}>
+            {data[2]}
+          </div>
+
+          <div className='flex justify-center items-center w-[40px]  lg:w-[45px] rounded-lg bg-white'>
+            <p>2</p>
+          </div>
+        </div>
+        {solving && <div className=' p-2'>true!</div>}
+      </div>
+      <button className='w-[120px] p-[5px] border-2 bg-slate-300' onClick={goToHome}>Back to home</button>
+    </section>
+
+
 
   );
 }
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -12,16 +12,130 @@ export default function TasksStart() {
 
 
   return (
-    
 
-      <section className="flex flex-col w-[100%] gap-[20px] justify-start items-center 
+
+    <section className="flex flex-col w-[100%] gap-[20px] justify-start items-center 
         min-h-screen min-w-[100%] bg-gradient-to-tr from-slate-300 to-slate-100 gree
         p-4">
 
-        <h1 className=' text-center' >Стартовая страница для задач</h1>
-      </section>
+      <h1 className=' text-center' >
+        Здесь Вы можете ознакомится с кратким изложением теории по операторам в javaScript и
+        eventLoop, а на вкладках operators и eventloop можете закрепить и проверить свои знания.
+      </h1>
 
-    
+      <div className='flex w-full flex-col gap-4 items-center'>
+        <div className='border-b-2 border-gray-600 text-left w-full'>operators</div>
+
+        <div className='flex w-full border-b-2 border-gray-600 border-dashed pb-1'>
+          <div className='w-[10%] lg:w-[20%]'>
+            <p className='text-sm lg:text-lg'>&&</p>
+          </div>
+          <div className='w-[90%] lg:w-[80%]'>
+            <p className='text-sm lg:text-lg'>Оператор && &#40;логическое "и"&#41;
+              - Возвращает первое ложное значение, если такое есть, иначе возвращает последнее значение.
+              - Используется для проверки, что все условия истинны.
+              - Если первое значение ложное &#40;falsy&#41;, то оно будет возвращено, и последующие значения не будут проверяться. <br />
+              Ложное значение — это значение, которое в контексте логических операций рассматривается как "ложь".
+              В JavaScript и многих других языках программирования к ложным значениям относятся: false, 0, undefinded, null, NaN, '' - пустая строка
+              В примере ниже ложные значения будут <span className='text-red-600'>красного</span> цвета,
+              истенные <span className='text-green-500'>зеленого</span> цвета.
+            </p>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col items-center border-b-2 border-gray-600 pb-1'>
+          <div className='grid grid-cols-3 w-[60%] '>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-green-500'>2</p>
+              <p className=''>&&</p>
+              <p className='text-green-500'>3</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-green-500'>3</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-red-600'>0</p>
+              <p className=''>&&</p>
+              <p className='text-green-500'>3</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-red-600'>0</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-green-500'>3</p>
+              <p className=''>&&</p>
+              <p className='text-red-600'>null</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-red-600'>null</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-red-600'>0</p>
+              <p className=''>&&</p>
+              <p className='text-red-600'>null</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-red-600'>0</div>
+
+          </div>
+        </div>
+
+        <div className='flex w-full border-b-2 border-gray-600 border-dashed pb-1'>
+          <div className='w-[10%] lg:w-[20%]'>
+            <p className='text-sm lg:text-lg'>||</p>
+          </div>
+          <div className='w-[90%] lg:w-[80%]'>
+            <p className='text-sm lg:text-lg'>Оператор || &#40;логическое "или"&#41;
+
+              - Возвращает первое истинное значение, если такое есть, иначе возвращает последнее значение.
+              - Применяется для проверки нескольких условий.
+              - Если первое значение истинное &#40;truthy&#41;, то оно будет возвращено, и последующие значения не будут проверяться. </p>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col items-center border-b-2 border-gray-600 pb-1'>
+          <div className='grid grid-cols-3 w-[90%] lg:w-[60%]'>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-green-500'>2</p>
+              <p className=''>||</p>
+              <p className='text-green-500'>3</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-green-500'>2</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-red-600'>0</p>
+              <p className=''>||</p>
+              <p className='text-green-500'>3</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-green-500'>3</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-green-500'>3</p>
+              <p className=''>||</p>
+              <p className='text-red-600'>null</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-green-500'>3</div>
+
+            <div className='flex justify-center gap-1'>
+              <p className='text-red-600'>0</p>
+              <p className=''>||</p>
+              <p className='text-red-600'>null</p>
+            </div>
+            <div className='text-center'>return</div>
+            <div className='text-center text-red-600'>null</div>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
 
   );
 }
